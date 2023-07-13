8a8c3ec38c0055ffbf7ae49ce3c3c21c94258219
Subham Das <70996404+Subham-wq@users.noreply.github.com>
2023-07-13 10:04:15 +0530

    Create Decision_Tree.py

---
+++ Decision_Tree.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import plot_tree
+
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a decision tree classifier
+dt = DecisionTreeClassifier(criterion='entropy', random_state=42)
+
+# Fit the decision tree classifier to the training data
+dt.fit(X_train, y_train)
+
+# Evaluate the model on the testing data
+y_pred = dt.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {:.2f}".format(accuracy))
+
+# Plot the decision tree
+plot_tree(dt, feature_names=iris.feature_names, class_names=iris.target_names, filled=True)
